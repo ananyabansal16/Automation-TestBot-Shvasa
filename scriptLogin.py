06da9eb95c3e830ab2bd8cce7cdff63f08c81751
ananyabansal16 <ananya.bansal2003@gmail.com>
2024-07-16 01:06:57 +0530

    updated scriptLogin

--- scriptLogin.py
+++ scriptLogin.py
@@ -7,6 +7,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import ElementNotInteractableException, TimeoutException, ElementClickInterceptedException
 
 driver = webdriver.Chrome()
 
@@ -45,12 +46,14 @@ try:
     )
     email_input.click()
     email_input.send_keys(test_email)
+    time.sleep(2)
 
     password_input = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/div/input'))
     )
     password_input.click()
     password_input.send_keys(test_password)
+    time.sleep(2)
 
     login_submit_button = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.XPATH, '//*[@id="loginForm"]/div/div[4]/button/span[1]/div'))
@@ -58,13 +61,57 @@ try:
     login_submit_button.click()
     
     print("Logged In!")
+    time.sleep(5)
     
-    time.sleep(10)
+    # book_a_class_button = WebDriverWait(driver, 10).until(
+    #     EC.presence_of_element_located((By.XPATH, '/html/body/div[8]/div[3]/div/div[2]/div[4]/button/span[1]'))
+    # )
+    # book_a_class_button.click()
     
-    book_a_class_button = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, '/html/body/div[8]/div[3]/div/div[2]/div[4]/button/span[1]'))
-    )
-    book_a_class_button.click()
+    # buttons = WebDriverWait(driver, 10).until(
+    #     EC.presence_of_all_elements_located((By.XPATH, "//button[contains(., 'Book Class')]"))
+    # )
+
+    # booked_classes = 0
+    # max_classes_to_book = 3
+
+    # while booked_classes < max_classes_to_book:
+    #     for button in buttons:
+    #         try:
+    #             button.click()
+    #             print("Class booked successfully!")
+    #             booked_classes += 1
+    #             time.sleep(5)
+    #             if booked_classes >= max_classes_to_book:
+    #                 break
+    #         except (ElementClickInterceptedException, ElementNotInteractableException, TimeoutException) as e:
+    #             print(f"Skipping non-interactable button due to error: {str(e)}")
+    #             driver.execute_script("window.scrollBy(0, 100);")  # Scroll down a bit to try the next button
+    #             continue
+
+    #     if booked_classes < max_classes_to_book:
+    #         # Scroll down and find more "Book Class" buttons
+    #         driver.execute_script("window.scrollBy(0, 1000);")
+    #         time.sleep(5)
+    #         buttons = driver.find_elements(By.XPATH, "//button[contains(., 'Book Class')]")
+        
+    # print(f"Booked {booked_classes} classes successfully!")
+    # time.sleep(5)  
+    
+    # go_to_home_button = WebDriverWait(driver, 10).until(
+    #     EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div[1]/div[3]/div[2]/div/div[2]/button/span[1]/span'))
+    # )
+    # go_to_home_button.click()
+    # time.sleep(2)
+    
+    # cross_button = WebDriverWait(driver, 10).until(
+    #     EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div[1]/div/div[1]/div[1]'))
+    # )
+    # cross_button.click()
+    
+    # print("Finally home. :)")
+    # time.sleep(15)
+    
     
 except Exception as e:
     print(f"Error occurred: {str(e)}")
